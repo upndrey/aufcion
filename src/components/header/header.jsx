04d6b1d43d27e092280aa4b9bcfef5b728a1e45f
upndrey <upndrey@yandex.ru>
2021-03-03 19:32:52 +0700

    Добавлен sass и убраны лишние библиотеки

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 class Header extends React.Component{
       render(){
           return <h2>О сайте</h2>;
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styles from './main.module.css'
+import styles from './main.module.scss';
+
 class Main extends React.Component{
       render(){
-          return <div className={styles.myStyle}>Основная информация</div>;
+          return <div className={styles.myStyle}><div>test</div>Основная информация</div>;
       }
   }
 
